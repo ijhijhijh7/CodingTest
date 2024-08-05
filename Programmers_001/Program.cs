13776b02a529e04428c263e50feff2f2047f4168
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-08-05 16:35:55 +0900

    Initial commit

---
+++ Programmers_001/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Programmers_001
+{
+    // <summary>
+    // 2024년 8월 12일
+    // 문제 :
+    // URL :
+    // <summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "banana";
+            int[] answer = new int[s.Length];
+
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = -1;
+            }
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (s[i] == s[j])
+                    {
+                        answer[i] = (i - j);
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < answer.Length; i++)
+            {
+                Console.Write(answer[i] + " ");
+            }
+
+        }
+    }
+}
