bd9c3f539441b3ec2cc993371ff6a2829f114ff2
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-08-09 12:12:11 +0900

    Add Programmers_007
    중앙값 구하기

---
+++ Programmers_007/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Programmers_007
+{
+    // <summary>
+    // 2024년 8월 9일
+    // 문제 : 중앙값 구하기(level 0)
+    // URL : https://school.programmers.co.kr/learn/courses/30/lessons/120811
+    // <summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 1, 10, 11, 7};
+            int answer = 0;
+            int temp;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1 - i; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(array[array.Length / 2]);
+            /*for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }*/
+        }
+    }
+}
